8c2fc49e932aa8de25b10a1b375eab3ad60ab674
naveenkumarhh5557-maker <naveenkumarhh5557@gmail.com>
2026-08-27 12:06:25 +0530

    Update solution.sql

--- Assignment09_Inner_Join/solution.sql
+++ Assignment09_Inner_Join/solution.sql
@@ -1,7 +1,25 @@
--- Create Department
+CREATE TABLE Department (
+    DepartmentID NUMBER(5) PRIMARY KEY,
+    DepartmentName VARCHAR2(30)
+);
 
--- Create Student
+CREATE TABLE Student (
+    StudentID NUMBER(5) PRIMARY KEY,
+    StudentName VARCHAR2(20),
+    DepartmentID NUMBER(5),
+    FOREIGN KEY (DepartmentID) REFERENCES Department(DepartmentID)
+);
 
--- Insert sample records
+INSERT INTO Department VALUES (101, 'Computer Science');
+INSERT INTO Department VALUES (102, 'Mathematics');
+INSERT INTO Department VALUES (103, 'Physics');
 
--- INNER JOIN query
+INSERT INTO Student VALUES (1001, 'Arun', 101);
+INSERT INTO Student VALUES (1002, 'Divya', 102);
+INSERT INTO Student VALUES (1003, 'Karthik', 101);
+INSERT INTO Student VALUES (1004, 'Nisha', 103);
+
+SELECT Student.StudentName, Department.DepartmentName
+FROM Student
+INNER JOIN Department
+ON Student.DepartmentID = Department.DepartmentID;
